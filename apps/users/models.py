53377624019e5a348f1ecfd35e3f1c3692d7dff5
v1r7 <gubaevmikhail@gmail.com>
2021-05-03 21:03:30 +0300

    added admin and some models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Models):
+    first_name = models.CharField(max_lenght=255)
+    last_name = models.CharField(max_lenght=255)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def ___str___(self):
+        return f'{self.last_name.capitalize} {self.first_name.capitalize}'
